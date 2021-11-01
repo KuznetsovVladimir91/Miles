a2598ae043a94210702e2a04b3708b606c7c3564
Vladimir Kuznetsov <kuznetsov.vladimir.91@mail.ru>
2021-11-01 14:04:26 +0300

    src/ added

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 1000;
+        byte milePrice = 20;
+        int bonusMiles = ticketPrice / milePrice;
+        System.out.println(bonusMiles);
+    }
+}
